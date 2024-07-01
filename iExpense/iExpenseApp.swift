b915c62b50513b760a8aa8105ad5a8c7e9ae4bab
Anthony Candelino <anthcandelino@gmail.com>
2024-07-01 09:41:57 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Anthony Candelino on 2024-07-01.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
