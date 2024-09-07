66a99788dc0522831ccbf3cdd365f0436087708d
Anthony Candelino <anthcandelino@gmail.com>
2024-09-06 22:11:21 -0400

    Upgrade app to use SwiftData

---
+++ iExpense/ExpenseItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseItemsView.swift
+//  iExpense
+//
+//  Created by Anthony Candelino on 2024-09-06.
+//
+
+import SwiftUI
+
+struct ExpenseItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseItemsView()
+}
